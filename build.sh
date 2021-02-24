d257508d50127e2d790cf2078d23abbaab37b56a
dtechsrv <dee.gabor@gmail.com>
2021-02-24 10:09:31 +0100

    fix aarch64 build issues

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Create image for Mecool/Videostrong KI Pro (gxl_p230_k1_pro)
 # Specs: Amlogic S905D SoC, 2 GB MB RAM, 16 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
 # Tested and works on PCB: 'K3_DVB_DDR4-V1.2', wireless module: ap6255.
-PROJECT=S905 DEVICE=KI-Pro ARCH=arm make image -j8
+PROJECT=S905 DEVICE=KI-Pro ARCH=aarch64 make image -j8
 
 # Create image for S8X2/M8S+ (m8m2_KII)
 # Specs: Amlogic S812 SoC, 2 GB RAM, 8 GB NAND Flash, 1 Gbps Ethernet (RTL8211F),
